7a08173daa65f0046cd9969fb70c490633eb7506
Scott Tawse <stawse99@protonmail.com>
2021-11-15 19:57:16 -0500

    Completed section 3, 4 and started 5

--- Section 1: Control Flow/3. Boolean Variables.py
+++ Section 1: Control Flow/3. Boolean Variables.py
@@ -1,1 +1,21 @@
 # Boolean Variables
+# True and False are the only bool types.
+# Any variable that is assigned one of these values is called a boolean variable.
+
+# Boolean variables can be created by assigning the True or False to a variable.
+# Example (bool)
+my_baby_bool = True
+# This will check if it is a boolean variable
+type(my_baby_bool)
+# This will print the results.
+print(type(my_baby_bool))
+
+# This example will show what is not a boolean variable.
+# Example (str)
+my_baby_bool_two = "true"
+# This will check if it is a boolean variable
+type(my_baby_bool_two)
+# Prints the results.
+print(type(my_baby_bool_two))
+
+print("Essentially a boolean variable can either be True or False and defined like such.")
---
+++ Section 1: Control Flow/4. If Statement.py
@@ -0,0 +1,13 @@
+# If Statement
+# Boolean variables/expressions are building blocks for conditional statements.
+# Example "If it is raining, then bring and umbrella."
+# This is a conditional statement True/False.
+# "it is raining" Is the boolean expression.
+
+# Example
+# Defines username as Dave
+user_name = "Dave"
+# whatever user_name is defined as will do what is indented.
+if user_name == "Dave":
+# So if user_name is defined as Dave then itll print this statement.
+  print("Get off my computer Dave!")
