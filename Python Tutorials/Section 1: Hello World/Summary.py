cae86064cce1758747fda6416288ee8e7a503281
Scott Tawse <stawse99@protonmail.com>
2021-11-16 19:38:42 -0500

    Started Section 3

---
+++ Python Tutorials/Section 1: Hello World/Summary.py
@@ -0,0 +1,68 @@
+# Summary
+# I am just goihng to summarize each section into one script.
+# Just because there is little info going on in each one.
+
+# 1. Comments
+# Comments are made by using the # at the beginning of the line or comment.
+
+# 2. Print
+# You can print onto the console by typing print.
+# Example
+print('Hello World!')
+
+# 3. Strings
+# Strings are a sentence in quotes '', "".
+#Example
+china = 'Dank country yaes'
+print(china)
+print('strings can be written in print or used to define variables.')
+
+# 4. Variables
+# Speaking of variables
+# Example
+dis_is_variable = "Something"
+numbvariable = 2
+
+# 5. Modulo
+# Skipped a couple things but its alot of basics you know already.
+# Modulo operator is indicated by % and gives the remainder of a division calculation.
+# This is something I didnt learn before.
+# Example
+print(29%5) # Remainder is 4 so answer is 4
+
+# 6. Concatenation
+# Simply allows you to put together several strings
+# Example
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+#print(message)
+print(message)
+
+# 7. Plus Equals
+# This allows you to add onto currently defined variables.
+# Instead of overwriting them.
+# Example
+miles_hiked = 12
+print(miles_hiked) #12
+miles_hiked += 5
+print(miles_hiked) #17
+
+# 8 Multi-line Strings
+# Lastly is by using three quote-marks (""" or ''') instead of one, 
+# enables for use of multiple lines until """ is presented again.
+
+# Example 
+to_you = """Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?"""
+
+
+print(to_you)
