aeb7766ba3c7bab5fdb7b2f73e72b5b08ef87e17
Scott Tawse <stawse99@protonmail.com>
2021-11-19 22:48:13 -0500

    Busy day. Only did 3.2.4 and 3.2.5 today

--- Python Tutorials/Section 3: Lists/2. Working with Lists in Python/4. Consecutive Lists: Range.py
+++ Python Tutorials/Section 3: Lists/2. Working with Lists in Python/4. Consecutive Lists: Range.py
@@ -1,1 +1,10 @@
 # Consecutive Lists: Range
+# Per say you need to type a list like so...
+# my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Instead of typing it out you can use th range()
+# function to do it for you.
+# Example
+my_range = range(10)
+print(my_range)
+number_list = range(9)
+print(list(number_list))
---
+++ Python Tutorials/Section 3: Lists/2. Working with Lists in Python/5. The Power of Range.py
@@ -0,0 +1,12 @@
+# The Power of Range!
+# With two inputs, we can create a list that starts at a different number.
+# Example
+my_list = range(2, 9)
+print(list(my_list))
+
+# You can also specify to skip in increments
+#Example
+range_five_three = range(5, 15, 3)
+print(range_five_three)
+range_diff_five = range(0,40,5)
+print(range_diff_five)
